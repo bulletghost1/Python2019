805093e6e3e55ec8fb34fade64817a147cf9fe3e
bulletghost1 <42982213+bulletghost1@users.noreply.github.com>
2020-01-17 13:57:44 -0800

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -1,4 +1,38 @@
 # jb hello.py
-word1 = input ("Please input your first name:")
-word2 = input ("Please input your last name:")
-print (word2 + ", " + word1)
+fname = input("What is your first name? ")
+print (fname)
+lname = input("What is your last name? ")
+print (lname)
+print ("Hi there, " + fname + " " + lname + " nice to meet you!")
+
+age = input("How old are you? ")
+print (age)
+print (str(age) + " is a good age")
+if (int(age) < 16): 
+	print("but you can't get your license") 
+elif (int(age) >= 16):	
+	print ("and you can get your license")
+else:
+	print ("ok boomer")
+
+hry = input("So " + fname + ", how are you today? ")
+print (hry)
+print ("You are "+ hry + ".")
+z = random.ranint(1,3)
+if (z = 1):
+	print
+elif (z == 2):
+	
+else:
+	
+
+print ("That is interesting to hear.")
+tell = input("Tell me more. ")
+if (tell == "sad" ):
+	print ("Oh thats bad to hear")
+elif (tell == "happy"):
+	print (" thats good to hear")
+else:
+	print("Sorry, I don't know that feeling")
+print("Well " + fname + ", it has been nice chatting with you.")
+
